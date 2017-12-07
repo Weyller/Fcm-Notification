d79a3ea5640afd1186a0c09e064eda2027f5af14
weyller <weyller@gmail.com>
2017-12-06 23:48:03 -0500

    clickable linear layout

--- app/src/main/java/ca/qc/lbpsb/fusion/fcmnotification/ChannelsActivity.java
+++ app/src/main/java/ca/qc/lbpsb/fusion/fcmnotification/ChannelsActivity.java
@@ -1,13 +1,44 @@
 package ca.qc.lbpsb.fusion.fcmnotification;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class ChannelsActivity extends AppCompatActivity {
 
+    LinearLayout layout;
+
+    ToggleButton toggleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channels);
+
+        toggleButton = findViewById(R.id.sw_fusion);
+
+        layout = findViewById(R.id.linear_update);
+        layout.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                      if(toggleButton.isChecked()){
+                          toggleButton.setChecked(false);
+                          view.setBackgroundColor(getResources().getColor(R.color.colorBack));
+                      } else {
+                          toggleButton.setChecked(true);
+
+                          view.setBackgroundColor(Color.rgb(132,155,175));
+                      }
+
+                       // view.setBackgroundColor(Color.rgb(132,155,175));
+                        Log.d("TAG_LAYOUT", "layaout : " + view.getTag());
+                    }
+                }
+        );
     }
 }
